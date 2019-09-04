6beb6215ac222995a5d32b86952dab041df294db
Yoan Sallami <ysallami@laas.fr>
2019-09-04 09:49:12 +0200

    fix area monitor

--- scripts/area_monitor.py
+++ scripts/area_monitor.py
@@ -81,14 +81,14 @@ class AreaMonitor(ReconfigurableClient):
                 del self.previously_inside_area[node_id]
 
             if node_id in self.currently_inside_area:
-                if node.id+"isInsideArea"+self.previously_inside_area[node.id] in self.predicates_map:
-                    fact_id = self.predicates_map[node.id+"isInsideArea"+self.previously_inside_area[node.id]]
+                if node_id+"isInsideArea"+self.previously_inside_area[node_id] in self.predicates_map:
+                    fact_id = self.predicates_map[node_id+"isInsideArea"+self.previously_inside_area[node_id]]
                     fact = self.ctx.worlds()[self.output_world].timeline().situations()[fact_id]
                     if fact is not None:
-                        print("stop : human-"+node.id+" is inside area "+ self.previously_inside_area[node.id])
+                        print("stop : human-"+node_id+" is inside area "+ self.previously_inside_area[node_id])
                         fact.end.data = now
                         changes.situations_to_update.append(fact)
-                        del self.predicates_map[node.id+"isInsideArea"+self.previously_inside_area[node.id]]
+                        del self.predicates_map[node_id+"isInsideArea"+self.previously_inside_area[node_id]]
 
         for node in self.ctx.worlds()[world_name].scene().nodes():
             if self.ctx.worlds()[world_name].scene().nodes().get_node_property(node.id, "class") == "Human":
